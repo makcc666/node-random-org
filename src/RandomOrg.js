af7ca12cef3346afd697ebc5bd08d565b9629939
Max <makcc666@users.noreply.github.com>
2017-12-27 08:13:18 +0300

    promisification

--- src/RandomOrg.js
+++ src/RandomOrg.js
@@ -1,73 +1,71 @@
-var Promise = require('bluebird');
-var url = require('url');
-var makeRpcRequest = require('./rpc').makeRpcRequest;
+const makeRpcRequest = require('./rpc').makeRpcRequest;
+const url = require("url");
 
 function RandomOrg(opts) {
-  this.apiKey = opts.apiKey;
-  if (typeof this.apiKey !== 'string') {
-    throw new Error('options.apiKey is required & must be a String');
-  }
-
-  this.endpoint = opts.endpoint || 'https://api.random.org/json-rpc/1/invoke';
-  this.endpoint = url.parse(this.endpoint);
-
-  // For testing.
-  this._makeRpcRequest = makeRpcRequest;
-}
-
-[
-  // Basic api methods
-  'generateIntegers',
-  'generateDecimalFractions',
-  'generateGaussians',
-  'generateStrings',
-  'generateUUIDs',
-  'generateBlobs',
-  'getUsage',
-  // Signed api methods
-  'generateSignedIntegers',
-  'generateSignedDecimalFractions',
-  'generateSignedGaussians',
-  'generateSignedStrings',
-  'generateSignedUUIDs',
-  'generateSignedBlobs',
-  'verifySignature'
-].forEach(function(methodName) {
-  RandomOrg.prototype[methodName] = createInvocation(methodName);
-});
-
-RandomOrg.prototype._enrichParams = function(method, params) {
-  if (method === 'verifySignature') {
-    /* The verifySignature method requires no api key (so that anyone
-     * can verify the authenticity of some response). */
-    return params;
-  }
-
-  var requestParams = { apiKey: this.apiKey };
-  Object.keys(params || {}).forEach(function(property) {
-    requestParams[property] = params[property];
-  });
-  return requestParams;
-}
-
-function createInvocation(methodName) {
-  return Promise.method(function(params) {
-    var requestOpts = {
-      endpoint: this.endpoint,
-      method: methodName,
-      params: this._enrichParams(methodName, params)
-    }
-    return this._makeRpcRequest(requestOpts)
-    .then(function (response) {
-      if (response.error) {
-        var error = new Error(response.error.message);
-        error.code = response.error.code;
-        throw error;
-      } else {
-        return response.result;
-      }
+    /*if (typeof this.apiKey !== 'string') {
+        throw new Error('options.apiKey is required & must be a String');
+    }*/
+    this.apiKey = opts.apiKey || "00000000-0000-0000-0000-000000000000";
+    this.endpoint = opts.endpoint || 'https://api.random.org/json-rpc/1/invoke';
+    this.endpoint = url.parse(this.endpoint);
+    
+    // For testing.
+    this._makeRpcRequest = makeRpcRequest;
+    
+    this._enrichParams = (method, params) => {
+        //console.log("RandomOrg.prototype._enrichParams::", method, params);
+        if (method === 'verifySignature') {
+            /* The verifySignature method requires no api key (so that anyone
+             * can verify the authenticity of some response). */
+            return params;
+        }
+        
+        let requestParams = {apiKey: this.apiKey};
+        Object.keys(params || {}).forEach(function (property) {
+            let this_param = params[property];
+            if (property === "proxy" && (this_param.toLowerCase().indexOf("http") !== 0)) {
+                this_param = "http://" + this_param;
+            }
+            requestParams[property] = this_param;
+        });
+        return requestParams;
+    };
+    
+    this.createInvocation = async function (methodName, params) {
+        let requestOpts = {
+            endpoint: this.endpoint,
+            method: methodName,
+            params: this._enrichParams(methodName, params)
+        };
+        return this._makeRpcRequest(requestOpts);
+    };
+    
+    let self = this;
+    
+    [
+        // Basic api methods
+        'generateIntegers',
+        'generateDecimalFractions',
+        'generateGaussians',
+        'generateStrings',
+        'generateUUIDs',
+        'generateBlobs',
+        'getUsage',
+        // Signed api methods
+        'generateSignedIntegers',
+        'generateSignedDecimalFractions',
+        'generateSignedGaussians',
+        'generateSignedStrings',
+        'generateSignedUUIDs',
+        'generateSignedBlobs',
+        'verifySignature'
+    ].forEach(function (methodName) {
+        RandomOrg.prototype[methodName] = async function (params) {
+            return self.createInvocation(methodName, params)
+        };
     });
-  });
+    
+    return this;
 }
 
 module.exports = RandomOrg;
